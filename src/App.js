9d999d8e2f1f33915a2592f7482d6110a7f1c3aa
hdebasish <hdebasish@gmail.com>
2024-10-04 02:47:46 +0530

    "Added comments and whitespace to App.js and Tooltip.js files for better readability."

--- src/App.js
+++ src/App.js
@@ -1,34 +1,51 @@
+// Import the Tooltip component from the './component/Tooltip' file
 import { Tooltip } from './component/Tooltip';
+
+// Import the App.css file for styling
 import './App.css';
+
+// Import the useState hook from the 'react' library
 import { useState } from 'react';
 
+// Define the App function component
 function App() {
+  // Initialize a state variable 'tooltip' with a default value of false
+  const [tooltip, setTooltip] = useState(false);
 
-  const [ tooltip, setTooltip ] = useState(false);
-
+  // Define an options object with CSS position properties
   const options = {
     top: "0%",
     right: "0%",
     bottom: "0%",
     left: "110%",
-  }
+  };
 
+  // Define a function to show the tooltip when called
   const showTooltip = () => {
     setTooltip(true);
-  }
+  };
 
+  // Define a function to hide the tooltip when called
   const hideTooltip = () => {
     setTooltip(false);
-  }
+  };
 
-
+  // Return the JSX elements for the App component
   return (
     <div className="App">
-      <div className="tooltipText" onMouseOver={showTooltip} onMouseLeave={hideTooltip}>Hover over me!
-        {  tooltip && <Tooltip options={options}/>}
+      {/* Create a div element with a class of 'tooltipText' */}
+      <div className="tooltipText" 
+           // Call the showTooltip function when the mouse is hovered over this element
+           onMouseOver={showTooltip} 
+           // Call the hideTooltip function when the mouse is moved away from this element
+           onMouseLeave={hideTooltip}>
+        Hover over me!
+        {/* Conditionally render the Tooltip component if the 'tooltip' state is true */}
+        {tooltip && <Tooltip options={options}/>}
       </div>
     </div>
   );
 }
 
+// Export the App component as the default export
 export default App;
--- src/component/Tooltip.js
+++ src/component/Tooltip.js
@@ -1,19 +1,24 @@
+// Import the styles from the './tooltip.module.css' file
 import styles from "./tooltip.module.css"
 
+// Define the Tooltip function component
 export function Tooltip(props) {
+  // Destructure the options object from the props
+  const { top, right, bottom, left } = props.options;
 
-    const { top, right, bottom, left } = props.options;
+  // Create a style object with the CSS position properties
+  const style = {
+    top,
+    right,
+    bottom,
+    left
+  }
 
-    const style = {
-        top,
-        right,
-        bottom,
-        left
-    }
-
-    return (
-        <div style={style} className={styles.tooltip}>
-            Thanks for hovering, I'm a tooltip!
-        </div>
-    );
+  // Return the JSX elements for the Tooltip component
+  return (
+    // Create a div element with the 'tooltip' class from the styles
+    <div style={style} className={styles.tooltip}>
+      Thanks for hovering, I'm a tooltip!
+    </div>
+  );
 }
